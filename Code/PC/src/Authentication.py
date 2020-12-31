1b13ea0c99bdc84c96c240a4d0606853d4839c19
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-12-31 17:51:47 +0100

    Updated client-server.

--- Code/PC/src/Authentication.py
+++ Code/PC/src/Authentication.py
@@ -2,11 +2,19 @@ import ecdsa
 import socket
 import threading
 import time
+import hashlib
+from termcolor import cprint
+
+class InvalidMessage(Exception):
+    pass
 
 class Authentication:
-    CLIENT_PUBLIC_KEY = '98cedbb266d9fc38e41a169362708e0509e06b3040a5dfff6e08196f8d9e49cebfb4f4cb12aa7ac34b19f3b29a17f4e5464873f151fd699c2524e0b7843eb383'
+    CLIENT_PUBLIC_KEY = '9aedaaa1468e178426e7cfb7257bee4ee589106ded1f282bc2cde90ebaea07ca'+ \
+                        '83ff329740d42624069f77fa360a6c30d2fc26ed7ce9cc32ed97cd5865aaa3c6'
     USERNAME_CLIENT = 'raffaeledndm'
     PASSWORD_CLIENT = 'ciao'
+    NONCE_LENGTH = 16
+    SIGNATURE_LENGTH = 64
 
     def __init__(self, port):
         self.PORT = port
@@ -43,12 +51,40 @@ class Authentication:
                 print(e)
 
     def verification(self, client_sd, addr):
-        msg = client_sd.recv(132).decode('utf-8', 'ignore')
+        #Decoded Message
+        msg = ''
+
+        while(True):
+            char = client_sd.recv(1).decode('utf-8', 'ignore')
+            msg += char
+
+            if msg.endswith('\r\n'):
+                break
+
+        msg = msg[:-2]
+
+        if len(msg.split('\r\n'))>3:
+            raise InvalidMessage
+
+        #Encoded nonce and signature
+        nonce = client_sd.recv(self.NONCE_LENGTH)
+        signature = client_sd.recv(self.SIGNATURE_LENGTH)
+        cprint(msg, 'cyan')
+        cprint(nonce, 'blue')
+        cprint(signature, 'green')
+        
+        try:
+            verify_key = ecdsa.VerifyingKey.from_string(bytes.fromhex(self.CLIENT_PUBLIC_KEY), curve=ecdsa.SECP256k1)
+            hash_msg = hashlib.sha256(msg.encode()+nonce).hexdigest()
+            print(verify_key.verify(signature, hash_msg.encode()))
+
+        except ecdsa.keys.BadSignatureError:
+            return False
+        
         client_sd.send('OK\r\n'.encode())
         #vk = VerifyingKey.from_string(bytes.fromhex(), curve=ecdsa.SECP256k1)
         #vk.verify(bytes.fromhex(sig), message) # True
 
 
 with Authentication(8080) as a:
-    print('c')
     a.run()
--- Code/PC/src/SecureElement.py
+++ Code/PC/src/SecureElement.py
@@ -1,16 +1,22 @@
 import ecdsa
 import socket
 import uuid
-
+from termcolor import cprint, colored
+import hashlib
 
 class SecureElement:
-
+    # SECP256k1 is the Bitcoin elliptic curve
+    #PRIVATE_KEY = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1).to_string().hex()
+    #PUBLIC_KEY = PRIVATE_KEY.get_verifying_key().to_string().hex()
+    PRIVATE_KEY = '86d2c50e3e273a8c31a08cabac7d416d54868d07b756a3b263096ea87da90b1a'
+    PUBLIC_KEY = '9aedaaa1468e178426e7cfb7257bee4ee589106ded1f282bc2cde90ebaea07ca'+ \
+                 '83ff329740d42624069f77fa360a6c30d2fc26ed7ce9cc32ed97cd5865aaa3c6'
+    
     def __init__(self, IP_address, port):
-        # SECP256k1 is the Bitcoin elliptic curve
-        self.PRIVATE_KEY = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) 
-        self.PUBLIC_KEY = self.PRIVATE_KEY.get_verifying_key()
-        print(len(self.PUBLIC_KEY.to_string()))
-        print(self.PUBLIC_KEY.to_string())
+        print(colored('PRIVATE KEY length: ', 'blue')+str(len(self.PRIVATE_KEY)))
+        print(colored(' PUBLIC KEY length: ', 'blue')+str(len(self.PUBLIC_KEY)))
+        #cprint(self.PRIVATE_KEY, 'green')
+        #cprint(self.PUBLIC_KEY, 'red')
 
         self.sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.IP_ADDRESS = IP_address
@@ -21,28 +27,20 @@ class SecureElement:
 
         return self
 
-    def sign(self, response):
-        self.sd.send(self.PUBLIC_KEY.to_string())
-        msg = self.sd.recv(4).decode('utf-8', 'ignore')
-        
-        if msg!='OK\r\n':
-            print('No correct sent of public key')
-            self.__exit__(None, None, None)
-
+    def sign(self, message):
         #Bytes object (sig)
-        sig = self.PRIVATE_KEY.sign(response.encode())
-        self.sd.send(sig)
+        sign_key = ecdsa.SigningKey.from_string(bytes.fromhex(self.PRIVATE_KEY), curve=ecdsa.SECP256k1)
+        cprint(message, 'cyan')
+        nonce=uuid.uuid4().bytes
+        cprint(nonce, 'blue')
+        hash_msg = hashlib.sha256(message.encode()+nonce).hexdigest()
+        signature = sign_key.sign(hash_msg.encode())
+        cprint(signature, 'green')
+        self.sd.send(message.encode()+b'\r\n'+nonce+signature)
         
-        print(uuid.uuid4())
-        
-        try:
-            return self.PUBLIC_KEY.verify(sig, b'ciao')
-        except ecdsa.keys.BadSignatureError:
-            return False
-
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.sd.close()
 
 
 with SecureElement('127.0.0.1', 8080) as s:
-    print(s.sign('ciao'))
+    s.sign('ciao')
